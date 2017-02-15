8e4356fae126522dd222c0d285e1b89a726162d1
tasconajoey <joey.tascona@gmail.com>
2017-02-15 16:42:41 -0500

    Finish product

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+$('.switch button').on('click', function() {
+  $(this).toggleClass('on off');
+  if ($(this).attr('class') === "on") {
 
-
+    // $(this).removeClass('on');
+    // $(this).addClass('off');
+    $('.status').text('Hey, who turned off the lights?');
+    $('body').attr('class', 'dark');
+  } else {
+    // $(this).removeClass('off');
+    // $(this).addClass('on');
+    $('.status').text('It\'s so bright in here!');
+    $('body').attr('class', 'light');
+  }
+});
